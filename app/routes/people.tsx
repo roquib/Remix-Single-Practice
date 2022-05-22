ed4d02fd0d61a6fb0c81682c3c4fa141a247e1a7
Abdur Roquib Pramanik <roquib01@outlook.com>
2022-05-22 13:59:27 +0600

    Remix Single: Multiple Forms and Single Button Mutations

--- app/routes/people.tsx
+++ app/routes/people.tsx
@@ -15,9 +15,16 @@ export const loader: LoaderFunction = async () => {
 };
 export const action: ActionFunction = async ({ request }) => {
   let formData = await request.formData();
-  let values = Object.fromEntries(formData);
-  const people = await db.people.create({ data: values });
-  return people;
+  let { _action, ...values } = Object.fromEntries(formData);
+  if (_action === "create") {
+    const people = await db.people.create({ data: values });
+    return people;
+  }
+  if (_action === "delete") {
+    return db.people.delete({
+      where: { id: values.id },
+    });
+  }
 };
 export default function Index() {
   let { people } = useLoaderData<LoaderData>();
@@ -29,14 +36,37 @@ export default function Index() {
         <ul>
           {people.map((person) => (
             <li key={person.id}>
-              {person.firstName} {person.lastName}
+              {person.firstName} {person.lastName}{" "}
+              <Form
+                style={{
+                  display: "inline",
+                }}
+                method="post"
+              >
+                <input type="hidden" name="id" value={person.id} />
+                <input
+                  type="hidden"
+                  name="createdAt"
+                  value={person.createdAt.toString()}
+                />
+                <button
+                  type="submit"
+                  aria-label="delete"
+                  name="_action"
+                  value="delete"
+                >
+                  x
+                </button>
+              </Form>
             </li>
           ))}
           <li>
             <Form method="post">
               <input type="text" name="firstName" />
               <input type="text" name="lastName" />
-              <button type="submit">Add</button>
+              <button type="submit" name="_action" value="create">
+                Add
+              </button>
             </Form>
           </li>
         </ul>
